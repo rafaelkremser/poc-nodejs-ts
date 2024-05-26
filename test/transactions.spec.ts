7ebde1d47fe2563c3b07f8e58d3d9cfe63717d89
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-26 02:52:31 -0300

    feat: create new test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -41,4 +41,58 @@ describe('Transactions routes', () => {
             expect.objectContaining({ title: 'New transaction', amount: 5000 }),
         ]);
     });
+
+    it('should be able to get a specific transaction', async () => {
+        const createdTransaction = await request(app.server)
+            .post('/transactions')
+            .send({ title: 'New transaction', amount: 5000, type: 'credit' });
+
+        const cookies = createdTransaction.get('Set-Cookie') ?? [];
+
+        const listTransactions = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200);
+
+        const transactionId = listTransactions.body.transactions[0].id;
+
+        const getTransactionById = await request(app.server)
+            .get(`/transactions/${transactionId}`)
+            .set('Cookie', cookies)
+            .expect(200);
+
+        expect(getTransactionById.body.transaction).toEqual(
+            expect.objectContaining({ title: 'New transaction', amount: 5000 })
+        );
+    });
+
+    it('should be able to get the summary', async () => {
+        const createdTransaction = await request(app.server)
+            .post('/transactions')
+            .send({
+                title: 'Credit transaction',
+                amount: 5000,
+                type: 'credit',
+            });
+
+        const cookies = createdTransaction.get('Set-Cookie') ?? [];
+
+        await request(app.server)
+            .post('/transactions')
+            .set('Cookie', cookies)
+            .send({
+                title: 'Debit transaction',
+                amount: 2000,
+                type: 'debit',
+            });
+
+        const summary = await request(app.server)
+            .get('/transactions/summary')
+            .set('Cookie', cookies)
+            .expect(200);
+
+        expect(summary.body.summary).toEqual({
+            amount: 3000,
+        });
+    });
 });
