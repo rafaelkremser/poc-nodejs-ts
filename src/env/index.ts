ba8b0dcc0d96aaa61d7dfe4676b8041b784c603b
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-26 15:16:20 -0300

    chore: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,13 +11,16 @@ const envSchema = z.object({
     NODE_ENV: z
         .enum(['development', 'test', 'production'])
         .default('production'),
+    DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
     DATABASE_URL: z.string(),
-    PORT: z.coerce.number().default(3333),
+    PORT: z.coerce.number().default(3000),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (_env.success === false) {
+    console.error('Invalid environment variables!', _env.error.format());
+
     throw new Error('Invalid environment variables.');
 }
 
