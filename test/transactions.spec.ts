0584be619c2470356ab84a5d00451694bc7336e9
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-26 02:18:56 -0300

    feat: create first tests

---
+++ test/transactions.spec.ts
@@ -0,0 +1,38 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import request from 'supertest';
+import { app } from '../src/app';
+
+describe('Transactions routes', () => {
+    beforeAll(async () => {
+        await app.ready();
+    });
+
+    afterAll(async () => {
+        await app.close();
+    });
+
+    it('should be able to create a new transaction', async () => {
+        const transaction = await request(app.server)
+            .post('/transactions')
+            .send({ title: 'New transaction', amount: 5000, type: 'credit' });
+
+        expect(transaction.statusCode).toEqual(201);
+    });
+
+    it('should be able to list all transactions', async () => {
+        const createdTransaction = await request(app.server)
+            .post('/transactions')
+            .send({ title: 'New transaction', amount: 5000, type: 'credit' });
+
+        const cookies = createdTransaction.get('Set-Cookie') ?? [];
+
+        const listTransactions = await request(app.server)
+            .get('/transactions')
+            .set('Cookie', cookies)
+            .expect(200);
+
+        expect(listTransactions.body.transactions).toEqual([
+            expect.objectContaining({ title: 'New transaction', amount: 5000 }),
+        ]);
+    });
+});
