f938007623777593788e8636ce15d7201b8826c9
Rafael Freitas Kremser <rafaelfreitaskermser@gmail.com>
2024-05-26 02:42:46 -0300

    feat: add new test

--- test/transactions.spec.ts
+++ test/transactions.spec.ts
@@ -1,6 +1,7 @@
-import { afterAll, beforeAll, describe, expect, it } from 'vitest';
+import { afterAll, beforeAll, beforeEach, describe, expect, it } from 'vitest';
 import request from 'supertest';
 import { app } from '../src/app';
+import { execSync } from 'node:child_process';
 
 describe('Transactions routes', () => {
     beforeAll(async () => {
@@ -9,6 +10,11 @@ describe('Transactions routes', () => {
 
     afterAll(async () => {
         await app.close();
+    });
+
+    beforeEach(() => {
+        execSync('npm run knex migrate:rollback --all');
+        execSync('npm run knex migrate:latest');
     });
 
     it('should be able to create a new transaction', async () => {
